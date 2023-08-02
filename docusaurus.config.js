a8c0f8084808afcbbfdb270eb3b69bd91c553111
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2023-08-02 16:25:00 +0300

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,8 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/silviuaavram/silviuaavram/tree/main',
-          postsPerPage: 'ALL'
+          blogSidebarTitle: 'Recent posts',
+          blogSidebarCount: 'ALL',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
